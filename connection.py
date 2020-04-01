261dac83032e1d512bc8cb683f2ee69cedc80ae6
sumeet <sumeet.somu@gmail.com>
2020-04-01 22:38:27 +0530

    dummy update gps function, handles status rfid attributes

--- connection.py
+++ connection.py
@@ -1,11 +1,11 @@
 from firebase_admin import db
 import datetime
 
-TABLE_VEHICLES_CONST = ""
+TABLE_VEHICLES_CONST = "/vehicles"
 
 #as of now, we are operating only on 1 vehicle
 #so this is the id of the object we are working on
-GAADI_CONST = ""
+GAADI_CONST = "KA05JX7838"
 
 def connect():
     print("trying to connect to table vehicles\n")
@@ -24,25 +24,42 @@ def updating_gps(vehicles, gaadi, latitude, longitude):
             "latitude": latitude,
             "longitude": longitude
         })
-        print("updated gps at ", datetime.datetime.now())
+        print("updated latitude: " + str(latitude) + ", longitude: " + str(longitude) + " at ", datetime.datetime.now())
+
+        if(vehicle_child.get()["rfid"].lower() == "locked"):
+            vehicle_child.update({
+                "status": "alert"
+            })
         
         if(vehicle_child.get()["status"].lower() == "alert"):
             print("send photo")
 
 def get_gps(vehicles):
+    vehicle_child = vehicles.child(GAADI_CONST)
+    latitude = vehicle_child.get()["latitude"]
+    longitude = vehicle_child.get()["longitude"]
+
     while True:
-        """
-        fill this function with your code to get gps coordinates
-        vehicles is the json file consiting the gaadi details
-        """
-        updating_gps(vehicles, GAADI_CONST, latitude, longitude)
+        dir = input()
+        if dir == "w":
+            longitude += 5
+        elif dir == "s":
+            longitude -= 5
+        elif dir == "d":
+            latitude += 5
+        elif dir == "a":
+            latitude -= 5
+        else:
+            pass
+
+        updating_gps(vehicles, GAADI_CONST, latitude = latitude, longitude = longitude)
 
 def updating_rfid(vehicles, gaadi, rfid_status):
     vehicle_child = vehicles.child(gaadi)
     vehicle_child.update({
         "rfid": rfid_status
     })
-    print("updated rfid at ", datetime.datetime.now())
+    #print("updated rfid at ", datetime.datetime.now())
 
 def get_rfid(vehicles):
     while True:
@@ -52,7 +69,7 @@ def get_rfid(vehicles):
         put an if statement, that if the rfid is detected,
         then only call updating_rfid
         """
-        updating_rfid(vehicles, GAADI_CONST, rfid_status)
+        updating_rfid(vehicles, GAADI_CONST, rfid_status = "LoCkEd")
 
 def printing(vehicles):
     print(vehicles.get())
