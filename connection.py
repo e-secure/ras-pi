3b8dd9fdb49bcc848f1b7cd4d69b8c8b99e2ecf0
Sumanthbhargava <sumanth1998@gmail.com>
2020-04-03 21:29:27 +0530

    Update

--- connection.py
+++ connection.py
@@ -1,11 +1,45 @@
 from firebase_admin import db
 import datetime
-
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
+import picamera
+import base64
 TABLE_VEHICLES_CONST = ""
 
 #as of now, we are operating only on 1 vehicle
 #so this is the id of the object we are working on
 GAADI_CONST = ""
+PASS=""
+
+def capture():
+    cam=picamera.PiCamera()
+    cam.resolution=(256,256)
+    cam.brightness=60
+    cam.start_preview()
+    cam.capture('img.jpeg')
+    cam.stop_preview()
+    img=open('img.jpeg','rb')
+    bs64=base64.b64encode(img.read()).decode('ascii')
+    img.close()
+    cam.close()
+    return bs64
+
+def authenticate():
+    tag=SimpleMFRC522()
+    time.sleep(1)
+    try:
+        print("Place tag on the reader")
+        id, text=tag.read()
+        print(id)
+        print(text)
+        if text==PASS:
+            return 1
+        else:
+            return 0
+
+    finally:
+        GPIO.cleanup()
 
 def connect():
     print("trying to connect to table vehicles\n")
@@ -16,9 +50,10 @@ def connect():
 
 def updating_gps(vehicles, gaadi, latitude, longitude):
     vehicle_child = vehicles.child(gaadi)
+    vehicle_images=vehicle_child.child("images")
     current_latitude = vehicle_child.get()["latitude"]
     current_longitude = vehicle_child.get()["longitude"]
-
+    count=vehicle_child.get()["counter"]
     if(abs(current_latitude - latitude) > 10 or abs(current_longitude - longitude) > 10):
         vehicle_child.update({
             "latitude": latitude,
@@ -33,6 +68,15 @@ def updating_gps(vehicles, gaadi, latitude, longitude):
         
         if(vehicle_child.get()["status"].lower() == "alert"):
             print("send photo")
+            b64=capture()
+            img="img"+str(count)
+            vehicle_images.update({
+                img: b64
+            })
+            count=count+1
+            vehicle_child.update({
+                "counter": count
+            })
 
 def get_gps(vehicles):
     vehicle_child = vehicles.child(GAADI_CONST)
@@ -59,9 +103,11 @@ def updating_rfid(vehicles, gaadi, rfid_status):
     vehicle_child.update({
         "rfid": rfid_status
     })
-    #print("updated rfid at ", datetime.datetime.now())
+    print("updated rfid at ", datetime.datetime.now())
 
 def get_rfid(vehicles):
+    vehicle_child = vehicles.child(GAADI_CONST)
+    rfid_status = vehicle_child.get()["rfid"]
     while True:
         """
         fill this function with your code to get gps coordinates
@@ -69,7 +115,16 @@ def get_rfid(vehicles):
         put an if statement, that if the rfid is detected,
         then only call updating_rfid
         """
-        updating_rfid(vehicles, GAADI_CONST, rfid_status = "LoCkEd")
+        print("authenticating")
+        x=authenticate()
+        if x==1:
+            if rfid_status=="locked":
+                rfid_status="unlocked"
+            else:
+                rfid_status="locked"
+            updating_rfid(vehicles, GAADI_CONST, rfid_status)
+        else:
+            pass
 
 def printing(vehicles):
     print(vehicles.get())
